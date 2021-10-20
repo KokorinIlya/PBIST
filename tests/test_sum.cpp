22c6de525be103a3026458f69ec04dbbbc1a5328
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2021-10-20 14:31:10 +0300

    moved to header

--- tests/test_sum.cpp
+++ tests/test_sum.cpp
@@ -4,6 +4,6 @@
 TEST(sum, simple)
 {
     pasl::pctl::parray<int32_t> xs = { 2, 5, 1, 7, 42, -24 };
-    int32_t sum = calc_sum(xs);
+    int32_t sum = calc_sum_parallel(xs);
     ASSERT_EQ(33, sum);
 }
