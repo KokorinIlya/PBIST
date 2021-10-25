ba3a44bc6c6ef35cb3f4e5b22f3871a45c18d49e
Ilya Kokorin <kokorin.ilya.1998@gmail.com>
2021-10-25 14:48:43 +0000

    Corrected tree building

--- src/include/ist_internal/build.h
+++ src/include/ist_internal/build.h
@@ -19,7 +19,9 @@ std::unique_ptr<ist_internal_node<T>> do_build_from_keys(
     pasl::pctl::parray<T> const& keys, 
     uint64_t left, uint64_t right, uint64_t size_threshold)
 {
-    std::cout << "left = " << left << ", right = " << right << std::endl;
+    pasl::pctl::raw raw_marker;
+
+    //std::cout << "left = " << left << ", right = " << right << std::endl;
     assert(right >= left);
     if (left == right)
     {
@@ -30,6 +32,7 @@ std::unique_ptr<ist_internal_node<T>> do_build_from_keys(
 
     if (keys_count <= size_threshold)
     {
+        //std::cout << "Leaf node" << std::endl;
         pasl::pctl::parray<std::pair<T, bool>> reps(
             keys_count,
             [left, &keys](long rep_idx) 
@@ -39,71 +42,69 @@ std::unique_ptr<ist_internal_node<T>> do_build_from_keys(
             }
         );
         pasl::pctl::parray<std::unique_ptr<ist_internal_node<T>>> children(
-            0,
+            raw_marker, 0,
             [](long)
             {
-                return std::unique_ptr<ist_internal_node<T>>(nullptr);
+                return nullptr;
             }
         );
 
         return std::make_unique<ist_internal_node<T>>(std::move(reps), std::move(children));
     }
 
-    std::cout << "Inner node" << std::endl;
+    //std::cout << "Inner node" << std::endl;
     uint64_t block_size = static_cast<uint64_t>(std::sqrt(keys_count));
     uint64_t rep_size = keys_count / (block_size + 1);
-    std::cout << "block_size = " << block_size << ", rep_size = " << rep_size << std::endl;
+    //std::cout << "block size = " << block_size << ", rep_size = " << rep_size <<
+    //    ", keys count = " << keys_count << std::endl;
 
     // TODO: use raw allocated memory
     pasl::pctl::parray<std::pair<T, bool>> reps(rep_size);
-    std::cout << "reps.size() = " << reps.size() << std::endl;
     pasl::pctl::parray<std::unique_ptr<ist_internal_node<T>>> children(
-        rep_size + 1,
+        raw_marker, rep_size + 1,
         [](long)
         {
-            return std::unique_ptr<ist_internal_node<T>>(nullptr);
+            return nullptr;
         }
     );
-    std::cerr << "children.size() = " << children.size() << std::endl;
 
-    std::cout << "Before loop" << std::endl;
-    std::function<void(long)> loop_body = 
-        [block_size, size_threshold, right, rep_size, &reps, &children, &keys] (long child_idx)
-    {
-        if (child_idx <= rep_size)
+    // TODO: loop body is not constant, use Comp-based pfor
+    pasl::pctl::parallel_for<uint64_t, std::function<void(long)>>(
+        0, rep_size + 1,
+        [block_size, size_threshold, left, right, rep_size, &reps, &children, &keys] (long child_idx)
         {
-            uint64_t start_idx;
-            uint64_t end_idx;
-
-            if (child_idx == 0)
+            if (child_idx < rep_size)
             {
-                start_idx = 0;
-                end_idx = block_size;
+                uint64_t start_idx;
+                uint64_t end_idx;
+
+                if (child_idx == 0)
+                {
+                    start_idx = left;
+                    end_idx = left + block_size;
+                }
+                else
+                {
+                    start_idx = left + (child_idx - 1) * (block_size + 1) + block_size;
+                    end_idx = left + child_idx * (block_size + 1) + block_size;
+                }
+                //std::cout << "not last, start_idx = " << start_idx << ", end_idx = " << end_idx << std::endl;
+
+                T const& cur_rep = keys[end_idx];
+                reps[child_idx] = {cur_rep, true};
+                children[child_idx] = do_build_from_keys(keys, start_idx, end_idx, size_threshold);;
+
             }
             else
             {
-                start_idx = (child_idx - 1) * (block_size + 1) + block_size;
-                end_idx = child_idx * (block_size + 1) + block_size;
+                assert(child_idx == rep_size);
+                uint64_t start_idx = left + (child_idx - 1) * (block_size + 1) + block_size;
+                uint64_t end_idx = right;
+                //std::cout << "last, start_idx = " << start_idx << ", end_idx = " << end_idx << std::endl;
+                children[child_idx] = do_build_from_keys(keys, start_idx, end_idx, size_threshold);
             }
-            std::cout << "start_idx = " << start_idx << ", end_idx = " << end_idx << std::endl;
-
-            T const& cur_rep = keys[end_idx];
-            reps[child_idx] = {cur_rep, true};
-            children[child_idx] = do_build_from_keys(keys, start_idx, end_idx, size_threshold);;
-
         }
-        else
-        {
-            assert(child_idx == rep_size);
-            uint64_t start_idx = (child_idx - 1) * (block_size + 1) + block_size;
-            uint64_t end_idx = right;
-            std::cout << "start_idx = " << start_idx << ", end_idx = " << end_idx << std::endl;
-            children[child_idx] = do_build_from_keys(keys, start_idx, end_idx, size_threshold);
-        }
-    };
-
-    // TODO: loop body is not constant, use Comp-based pfor
-    pasl::pctl::parallel_for<uint64_t, std::function<void(long)>>(0, rep_size + 1, loop_body);
+    );
     return std::make_unique<ist_internal_node<T>>(std::move(reps), std::move(children));
 }
 
--- tests/test_tree_building.cpp
+++ tests/test_tree_building.cpp
@@ -3,7 +3,6 @@
 #include "ist_internal/build.h"
 #include <memory>
 #include <cstdint>
-
 #include <stdio.h>
 #include <execinfo.h>
 #include <signal.h>
@@ -15,11 +14,7 @@ void handler(int sig)
 {
     void *array[10];
     size_t size;
-
-    // get void*'s for all entries on the stack
     size = backtrace(array, 10);
-
-    // print out all the frames to stderr
     fprintf(stderr, "Error: signal %d:\n", sig);
     backtrace_symbols_fd(array, size, STDERR_FILENO);
     exit(1);
@@ -27,7 +22,7 @@ void handler(int sig)
 
 TEST(tree_building, simple)
 {
-    signal(SIGSEGV, handler);
+    //signal(SIGSEGV, handler);
     pasl::pctl::parray<int32_t> keys(
         100,
         [](long i)
