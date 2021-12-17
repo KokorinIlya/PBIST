07fe4a9e78983ea47c824373b75d4c1a1fde2932
Ilya Kokorin <kokorin.ilya.1998@gmail.com>
2021-12-17 10:27:06 +0000

    remove tests done

--- tests/test_remove.h
+++ tests/test_remove.h
@@ -112,3 +112,123 @@ TEST(remove, simple_batch_contains)
         ASSERT_EQ(expected_res[i], contains_res[i]);
     }
 }
+
+TEST(remove, empty)
+{
+    pasl::pctl::raw raw_marker;
+
+    ist_internal<int32_t> tree({}, 3);
+
+    pasl::pctl::parray<int32_t> keys_remove = {-100, 2, 7, 8, 9, 13, 15, 21, 50, 75, 100, 103};
+    pasl::pctl::parray<bool> remove_res = tree.remove(keys_remove);
+    ASSERT_EQ(keys_remove.size(), remove_res.size());
+    for (uint64_t i = 0; i < remove_res.size(); ++i)
+    {
+        ASSERT_FALSE(remove_res[i]);
+    }
+
+    pasl::pctl::parray<int32_t> keys_to_check(
+        raw_marker, 2000,
+        [](uint64_t idx)
+        {
+            return idx - 1000;
+        }
+    );
+    pasl::pctl::parray<bool> contains_res = tree.contains(keys_to_check);
+    ASSERT_EQ(keys_to_check.size(), contains_res.size());
+    for (uint64_t i = 0; i < contains_res.size(); ++i)
+    {
+        ASSERT_FALSE(contains_res[i]);
+    }
+}
+
+TEST(remove, single_remove_stress)
+{
+    uint32_t max_tree_size = 100'000;
+    uint32_t max_req_size = 10'000;
+
+    std::default_random_engine generator(time(nullptr));
+    std::uniform_int_distribution<uint32_t> tree_size_distribution(1, max_tree_size);
+    std::uniform_int_distribution<uint32_t> req_size_distribution(1, max_req_size);
+    std::uniform_int_distribution<int32_t> elements_distribution(-100'000, 100'000);
+    std::uniform_int_distribution<uint32_t> size_threshold_distribution(3, 10);
+
+    for (uint32_t i = 0; i < TESTS_COUNT; ++i)
+    {
+        uint32_t cur_tree_size = tree_size_distribution(generator);
+        uint32_t cur_size_threshold = size_threshold_distribution(generator);
+
+        auto [keys_set, keys] = get_build_batch<int32_t>(cur_tree_size, generator, elements_distribution);
+        ist_internal<int32_t> tree(keys, cur_size_threshold);
+
+        uint32_t cur_remove_size = req_size_distribution(generator);
+        auto [exp_remove_res, remove_keys] = get_remove_batch<int32_t>(
+            keys_set, cur_remove_size, generator, elements_distribution
+        );
+        pasl::pctl::parray<bool> remove_res = tree.remove(remove_keys);
+        ASSERT_EQ(exp_remove_res.size(), remove_res.size());
+        for (uint64_t i = 0; i < remove_res.size(); ++i)
+        {
+            ASSERT_EQ(exp_remove_res[i], remove_res[i]);
+        }
+
+        uint32_t cur_contains_size = req_size_distribution(generator);
+        auto [exp_contains_res, contains_keys] = get_contains_batch<int32_t>(
+            keys_set, cur_contains_size, generator, elements_distribution
+        );
+
+        pasl::pctl::parray<bool> contains_res = tree.contains(contains_keys);
+        ASSERT_EQ(exp_contains_res.size(), contains_res.size());
+        for (uint64_t i = 0; i < contains_res.size(); ++i)
+        {
+            ASSERT_EQ(exp_contains_res[i], contains_res[i]);
+        }
+    }
+}
+
+TEST(remove, multiple_insert_stress)
+{
+    uint32_t max_tree_size = 100'000;
+    uint32_t max_req_size = 10'000;
+
+    std::default_random_engine generator(time(nullptr));
+    std::uniform_int_distribution<uint32_t> tree_size_distribution(1, max_tree_size);
+    std::uniform_int_distribution<uint32_t> req_size_distribution(1, max_req_size);
+    std::uniform_int_distribution<int32_t> elements_distribution(-200'000, 200'000);
+    std::uniform_int_distribution<uint32_t> size_threshold_distribution(3, 10);
+
+    for (uint32_t i = 0; i < TESTS_COUNT; ++i)
+    {
+        uint32_t cur_tree_size = tree_size_distribution(generator);
+        uint32_t cur_size_threshold = size_threshold_distribution(generator);
+
+        auto [keys_set, keys] = get_build_batch<int32_t>(cur_tree_size, generator, elements_distribution);
+        ist_internal<int32_t> tree(keys, cur_size_threshold);
+
+        for (uint32_t j = 0; j < BATCH_REQUESTS_PER_TEST; ++j)
+        {
+            uint32_t cur_remove_size = req_size_distribution(generator);
+            auto [exp_remove_res, remove_keys] = get_remove_batch<int32_t>(
+                keys_set, cur_remove_size, generator, elements_distribution
+            );
+            pasl::pctl::parray<bool> remove_res = tree.remove(remove_keys);
+            ASSERT_EQ(exp_remove_res.size(), remove_res.size());
+            for (uint64_t i = 0; i < remove_res.size(); ++i)
+            {
+                ASSERT_EQ(exp_remove_res[i], remove_res[i]);
+            }
+
+            uint32_t cur_contains_size = req_size_distribution(generator);
+            auto [exp_contains_res, contains_keys] = get_contains_batch<int32_t>(
+                keys_set, cur_contains_size, generator, elements_distribution
+            );
+
+            pasl::pctl::parray<bool> contains_res = tree.contains(contains_keys);
+            ASSERT_EQ(exp_contains_res.size(), contains_res.size());
+            for (uint64_t i = 0; i < contains_res.size(); ++i)
+            {
+                ASSERT_EQ(exp_contains_res[i], contains_res[i]);
+            }
+        }
+    }
+}
--- tests/test_utils.h
+++ tests/test_utils.h
@@ -12,7 +12,7 @@ std::pair<std::unordered_set<T>, pasl::pctl::parray<T>> get_build_batch(
     uint64_t batch_size,
     std::default_random_engine& generator, std::uniform_int_distribution<T>& elements_distribution) 
 {
-    std::vector<T> keys_v;
+    std::vector<T> keys_v; // TODO: common code
     std::unordered_set<T> keys_set;
     while (keys_v.size() < batch_size)
     {
@@ -116,6 +116,55 @@ std::pair<std::vector<bool>, pasl::pctl::parray<T>> get_insert_batch(
     {
         bool inserted = tree_keys.insert(keys[i]).second;
         exp_res.push_back(inserted);
+    }
+    
+    return {exp_res, keys};
+}
+
+template <typename T>
+std::pair<std::vector<bool>, pasl::pctl::parray<T>> get_remove_batch(
+    std::unordered_set<T>& tree_keys, uint64_t batch_size,
+    std::default_random_engine& generator, std::uniform_int_distribution<T>& elements_distribution) 
+{
+    std::vector<T> keys_v;
+    std::unordered_set<T> keys_set;
+
+    while (keys_v.size() < batch_size)
+    {
+        assert(keys_v.size() == keys_set.size());
+        int32_t cur_elem =  elements_distribution(generator);
+        if (keys_set.find(cur_elem) != keys_set.end())
+        {
+            continue;
+        }
+        auto insert_res = keys_set.insert(cur_elem);
+        assert(insert_res.second);
+        keys_v.push_back(cur_elem);
+    }
+    assert(keys_v.size() == keys_set.size());
+
+    std::sort(keys_v.begin(), keys_v.end());
+    pasl::pctl::parray<T> keys(
+        keys_v.size(),
+        [&keys_v](uint64_t i)
+        {
+            return keys_v[i];
+        }
+    );
+
+    std::vector<bool> exp_res;
+    for (uint64_t i = 0; i < keys.size(); ++i)
+    {
+        auto it = tree_keys.find(keys[i]);
+        if (it != tree_keys.end())
+        {
+            exp_res.push_back(true);
+            tree_keys.erase(it);
+        }
+        else
+        {
+            exp_res.push_back(false);
+        }
     }
     
     return {exp_res, keys};
